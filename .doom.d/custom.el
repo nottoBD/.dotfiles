69dfe78bd9662033f6bfb977b0baf977aa7200e2
David Botton <david.botton@mail.com>
2024-11-20 15:43:48 +0100

    Added Doom Emacs specifics

---
+++ .doom.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(djvu frecentf org-tree-slide-pauses pdf-tools org-tree-slide org-noter org-attach-screenshot @)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
